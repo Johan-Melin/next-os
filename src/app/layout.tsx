c638db07434a9a694cf61627aadd810cd577fc48
Johan Melin <soldierheart@hotmail.com>
2025-09-22 21:03:49 +0200

    style(dock): make dock floating for better desktop experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,10 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <div 
-          className="fixed bottom-0 left-0 right-0 flex justify-around border-t border-neutral-800" 
-          style={{ backgroundColor: "var(--background)"}}
-        >
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex justify-center items-end p-2 rounded-2xl backdrop-blur-lg border border-white/20">
           {dockApps.map((app) => (
             <AppIcon key={app.title} Icon={app.Icon} title={app.title} />
           ))}
