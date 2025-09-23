d044d94e03ebf60ea0d7c251e1613e011f686103
Johan Melin <soldierheart@hotmail.com>
2025-09-23 12:25:41 +0200

    feat(apps): add app interface (allows empty list)

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -1,13 +1,19 @@
+import { IconType } from 'react-icons';
 import { FiInfo, FiGlobe, FiGrid, FiSettings } from "react-icons/fi";
 
-export const apps = [
+export interface AppItem {
+  Icon: IconType;
+  title: string;
+}
+
+export const apps: AppItem[] = [
     { Icon: FiInfo, title: "info" },
     { Icon: FiGlobe, title: "news" },
     { Icon: FiGrid, title: "program" },
     { Icon: FiSettings, title: "settings" },
 ];
   
-export const dockApps = [
+export const dockApps: AppItem[] = [
   { Icon: FiInfo, title: "info" },
   { Icon: FiGlobe, title: "news" },
   { Icon: FiGrid, title: "program" },
