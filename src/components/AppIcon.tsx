606ed6757494654158450e383c43db7c7e2720b4
Johan Melin <soldierheart@hotmail.com>
2025-09-26 10:57:02 +0200

    feat(AppIcon): let long titles trail

--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -7,14 +7,14 @@ interface AppIconProps {
   onClick?: () => void;
 }
 
-export default function AppIcon({ Icon, title, target, onClick }: AppIconProps) {
+export default function AppIcon({ Icon, title, onClick }: AppIconProps) {
   return (
     <div 
       className="flex flex-col items-center justify-center h-14 w-14" 
       onClick={onClick}
     >
       <Icon size={24} />
-      <p className="text-xs pt-1">{title}</p>
+      <p className="text-xs pt-1 w-full text-center truncate">{title}</p>
     </div>
   );
 }
